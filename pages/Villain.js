0b4c5ea6c9dae8507897340e2a0e7c3a113fe3d9
Hannan1717 <hannanelang17@gmail.com>
2022-12-05 23:14:51 +0700

    initt

--- pages/Villain.js
+++ pages/Villain.js
@@ -1,29 +1,49 @@
 import { useEffect, useState } from 'react';
-import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
+import { View, Text, Image, FlatList, StyleSheet, TextInput } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 function Villain({ navigation }) {
-  const [data, setData] = useState([]);
-
+  const [filteredData, setFilteredData] = useState([]);
+  const [masterData, setMasterData] = useState([]);
+  const [search, setsearch] = useState('');
   useEffect(() => {
     getData();
+    return () => {};
   }, []);
 
   function getData() {
     fetch('http://localhost:3000/villain')
       .then((response) => response.json())
       .then((json) => {
-        setData(json);
+        setFilteredData(json);
+        setMasterData(json);
       })
       .catch((error) => {
         console.error(error);
       });
   }
 
+  const searchFilter = (text) => {
+    if (text) {
+      const newData = masterData.filter((item) => {
+        const itemData = item.nama ? item.nama.toUpperCase() : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredData(newData);
+      setsearch(text);
+    } else {
+      setFilteredData(masterData);
+      setsearch(text);
+    }
+  };
+
   return (
     <View style={{ flex: 1, marginTop: 20 }}>
+      <TextInput style={styles.searchbar} value={search} placeholder="Search here" underlineColorAndroid="transparent" onChangeText={(text) => searchFilter(text)}></TextInput>
       <FlatList
-        data={data}
+        data={filteredData}
+        keyExtractor={(item, index) => index.toString()}
         renderItem={({ item }) => (
           <TouchableOpacity onPress={() => navigation.navigate('Detail Villain', { msg: item.id })}>
             <View style={styles.oddItemListContainer}>
@@ -43,6 +63,18 @@ const styles = StyleSheet.create({
   flatlist: {
     flex: 1,
     marginHorizontal: 10,
+  },
+  searchbar: {
+    marginHorizontal: 20,
+    height: 45,
+    backgroundColor: '#FFFFFF',
+    marginVertical: 5,
+    borderRadius: 10,
+    borderColor: '#000000',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 40,
+    borderWidth: 1,
   },
   oddItemListContainer: {
     marginHorizontal: 20,
